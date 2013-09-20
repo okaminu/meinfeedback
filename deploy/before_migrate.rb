5b55da7b53031ff989aa33ab3ba72df3164612a0
Mazvydas Skuodas <mazvydas@foxrate.de>
2013-09-20 08:42:12 +0300

    deployment files

---
+++ deploy/before_migrate.rb
@@ -0,0 +1,57 @@
+node[:deploy].each do |application, deploy|
+#  Chef::Log.debug("")
+
+  file "#{release_path}/app/config/parameters.yml" do
+    database = deploy[:database]
+    content ERB.new(::File.read("#{release_path}/app/config/parameters.yml.dist")).result(binding)
+    action :create
+    mode "0644"
+  end
+
+end
+
+directory "#{release_path}/app/cache" do
+    mode 00777
+    owner "www-data"
+    group "www-data"
+    action :create
+    recursive true
+end
+
+directory "#{release_path}/app/logs" do
+    mode 00777
+    owner "www-data"
+    group "www-data"
+    action :create
+    recursive true
+end
+
+execute "install composer.phar" do
+    cwd release_path
+    command "curl -s http://getcomposer.org/installer | php"
+    action :run
+end
+
+execute "install composer.json (actually composer.lock)" do
+    cwd release_path
+    command "php composer.phar install --prefer-dist"
+    action :run
+end
+
+execute "prepare symfony cache for production" do
+    cwd release_path
+    command "php app/console cache:clear --env=prod"
+    action :run
+end
+
+execute "fix cache and logs owner" do
+    cwd release_path
+    command "chown -R www-data:www-data app/cache app/logs"
+    action :run
+end
+
+execute "fix cache and logs permissions" do
+    cwd release_path
+    command "chmod -R 0777 app/cache app/logs"
+    action :run
+end
