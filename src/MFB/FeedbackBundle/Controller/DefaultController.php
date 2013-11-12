a8225e652de7ecc3a2cedd3a4141c318e72c66f4
Okaminu <aurimasdgt@gmail.com>
2013-11-12 11:44:15 +0200

    MFB-13: form is rendered, controller refactored

--- src/MFB/FeedbackBundle/Controller/DefaultController.php
+++ src/MFB/FeedbackBundle/Controller/DefaultController.php
@@ -9,6 +9,8 @@ use MFB\AccountBundle\Entity\Account;
 use MFB\ChannelBundle\Entity\AccountChannel;
 use MFB\CustomerBundle\Entity\Customer;
 use MFB\CustomerBundle\Form\CustomerType;
+use Symfony\Component\Form\FormError;
+use Doctrine\DBAL\DBALException;
 
 class DefaultController extends Controller
 {
@@ -30,26 +32,17 @@ class DefaultController extends Controller
         $accountChannel = $em->getRepository('MFBChannelBundle:AccountChannel')->findOneBy(
             array('accountId'=>$account->getId())
         );
+
         if (!$accountChannel) {
             throw $this->createNotFoundException('Account does not have any channels');
         }
 
-        return $this->render(
-            'MFBFeedbackBundle:Default:index.html.twig',
-            array(
-                'accountId' => $account->getId(),
-                'accountChannelId' => $accountChannel->getId(),
-                'account_channel_name' => $accountChannel->getName(),
-                'ratingEnabled' => $accountChannel->getRatingsEnabled(),
-                'errorMessage' => false,
-                'feedback' => '',
-                'form' => $form->createView()
-            )
-        );
+        return $this->showFeedbackForm($account->getId(), $accountChannel, $form->createView());
     }
 
     public function saveAction(Request $request)
     {
+        $rating = null;
         $em = $this->getDoctrine()->getManager();
 
         /** @var Account $account */
@@ -64,71 +57,68 @@ class DefaultController extends Controller
             throw $this->createNotFoundException('Channel does not exist');
         }
 
-        //customer Id
-
-        $entity = new Customer();
-        $entity->setAccountId($account);
-        $form = $this->createForm(new CustomerType(), $entity);
+        $customer = new Customer();
+        $customer->setAccountId($account->getId());
+        $form = $this->createForm(new CustomerType(), $customer);
 
         $form->handleRequest($request);
 
         if ($form->isValid()) {
-            try
-            {
-                $em->persist($entity);
+            try {
+                $em->persist($customer);
                 $em->flush();
-                $customerId = $entity->getId();
 
+                $feedback = new Feedback();
+                $feedback->setAccountId($account->getId());
+                $feedback->setChannelId($accountChannel->getId());
+                $feedback->setCustomerId($customer->getId());
+                $feedback->setContent($request->get('feedback'));
 
+                $requestRating = (int)$request->get('rating');
 
-
-        $feedback = new Feedback();
-        $feedback->setAccountId($account);
-        $feedback->setChannelId($accountChannel);
-        $feedback->setCustomerId($customerId);
-        $feedback->setContent($request->get('feedback'));
-
-        $rating = null;
-
-        $requestRating = (int)$request->get('rating');
-
-        if (($requestRating > 0) && ($requestRating <= 5)) {
-            $rating = $requestRating;
-        }
-
-        if (($accountChannel->getRatingsEnabled() == '1') && (is_null($rating))) {
-
-            return $this->render(
-                'MFBFeedbackBundle:Default:index.html.twig',
-                array(
-                    array(
-                        'accountId' => $account->getId(),
-                        'accountChannelId' => $accountChannel->getId(),
-                        'account_channel_name' => $accountChannel->getName(),
-                        'ratingEnabled' => $accountChannel->getRatingsEnabled(),
-                        'errorMessage' => 'Please select star rating',
-                        'feedback' => $request->get('feedback'),
-                        'form' => $form->createView()
-                        )
-                    )
-            );
-        }
-
-        $feedback->setRating($rating);
-        $em->persist($feedback);
-        $em->flush();
-
-            }
-            catch (DBALException $ex) {
-                $ex = $ex->getPrevious();
-                if ($ex instanceof \PDOException && $ex->getCode() == 23000 ) {
-                    $form->get('email')->addError( new FormError('Email already exists'));
-                } else {
-                    $form->addError( new FormError($ex->getMessage()));
+                if (($requestRating > 0) && ($requestRating <= 5)) {
+                    $rating = $requestRating;
                 }
 
+                if (($accountChannel->getRatingsEnabled() == '1') && (is_null($rating))) {
+                    $this->showFeedbackForm(
+                        $account->getId(),
+                        $accountChannel,
+                        $form->createView(),
+                        $request->get('feedback'),
+                        'Please select star rating'
+                    );
+                }
+
+                $feedback->setRating($rating);
+                $em->persist($feedback);
+                $em->flush();
+
+            } catch (DBALException $ex) {
+                $ex = $ex->getPrevious();
+                if ($ex instanceof \PDOException && $ex->getCode() == 23000) {
+                    $form->get('email')->addError(new FormError('Email already exists'));
+                } else {
+                    $form->addError(new FormError($ex->getMessage()));
+                }
             }
+            return $this->render('MFBFeedbackBundle:Invite:thank_you.html.twig');
         }
-        return $this->render('MFBFeedbackBundle:Invite:thank_you.html.twig');
+        return $this->showFeedbackForm($account->getId(), $accountChannel, $form->createView());
+    }
+
+
+    private function showFeedbackForm($accountId, $accountChannel, $formView, $feedback = '', $starErrorMessage = false)
+    {
+        return $this->render(
+            'MFBFeedbackBundle:Default:index.html.twig',
+            array(
+                'accountId' => $accountId,
+                'accountChannel' => $accountChannel,
+                'starErrorMessage' => $starErrorMessage,
+                'feedback' => $feedback,
+                'form' => $formView
+            )
+        );
     }
 }
