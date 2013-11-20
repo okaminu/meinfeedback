4bf3880b559ea80daecb04b5107bf943681f84a1
Okaminu <aurimasdgt@gmail.com>
2013-11-20 11:53:52 +0200

    MFB-33: tooltip displayed near the widget box

--- src/MeinFeedback/HomeBundle/Resources/public/js/qtip.config.js
+++ src/MeinFeedback/HomeBundle/Resources/public/js/qtip.config.js
@@ -1,4 +1,20 @@
-$('.tip').each(function() { // Grab all elements with a title attribute,and set "this"
+$('.tip').each(function() {
+    bottomLeft = {
+            my: 'top right',
+            at: 'bottom left'
+                };
+
+    bottomRight = {
+        my: 'top left',
+        at: 'bottom right'
+    };
+    boxPosition = bottomRight;
+
+    if($(this).hasClass('positionBottomLeft')){
+        boxPosition = bottomLeft
+    }
+
+
     $(this).qtip({ //
         content: {
             text: $(this).next(), // WILL work, because .each() sets "this" to refer to each element
@@ -7,11 +23,11 @@ $('.tip').each(function() { // Grab all elements with a title attribute,and set 
                 button: 'Close' // Close button
             }
         },
+        position: boxPosition,
         style: 'qtip-light qtip-shadow',
         show: 'click',
         hide: {
-            event: 'click',
-            inactive: 1500
+            event: 'click'
         }
     });
 });
