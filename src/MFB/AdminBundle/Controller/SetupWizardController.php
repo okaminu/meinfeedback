4df9db2d41086a18f3c89fad58b7475050ad3a8c
Okaminu <aurimasdgt@gmail.com>
2014-01-29 10:59:33 +0200

    MFB-139: refactoring business selection step controller

--- src/MFB/AdminBundle/Controller/SetupWizardController.php
+++ src/MFB/AdminBundle/Controller/SetupWizardController.php
@@ -9,42 +9,16 @@ class SetupWizardController extends Controller
 {
     public function selectBusinessAction(Request $request)
     {
-        $businessList = $this->get('mfb_service_business.service')->findAll();
-
-        $choices = array();
-
-        foreach ($businessList as $business) {
-            $choices[$business->getId()] = $business->getName();
-        }
-
-        $builder = $this->createFormBuilder();
-        $builder->add(
-            'businessType',
-            'choice',
-            array('multiple' => false, 'expanded' => true, 'mapped' => false, 'choices' => $choices)
-        );
-        $builder->add('submit', 'submit', array('label' => 'Continue'));
-        $form = $builder->getForm();
-
+        $form = $this->createBusinessForm();
         $form->handleRequest($request);
 
         if ($form->isValid()) {
-            $value = $form->get('businessType')->getData();
-            return $this->redirect(
-                $this->generateUrl(
-                    'mfb_admin_setup_select_service_type',
-                    array('businessId' => $value)
-                )
+            return $this->createRedirect(
+                'mfb_admin_setup_select_service_type',
+                array('businessId' => $form->get('businessType')->getData())
             );
         }
-
-        return $this->render(
-            "MFBAdminBundle:SetupWizard:selectBusiness.html.twig",
-            array(
-                'businessList' => $businessList,
-                'form' => $form->createView()
-            )
-        );
+        return $this->showSelectBusinessForm($form);
     }
 
     public function selectServiceTypeAction($businessId)
@@ -53,5 +27,40 @@ class SetupWizardController extends Controller
         exit;
     }
 
+    private function createBusinessForm()
+    {
+        $businessList = $this->get('mfb_service_business.service')->findAll();
+        $choices = array();
+        foreach ($businessList as $business) {
+            $choices[$business->getId()] = $business->getName();
+        }
 
+        $builder = $this->createFormBuilder();
+        $builder->add(
+            'businessType',
+            'choice',
+            array(
+                'multiple' => false,
+                'expanded' => true,
+                'mapped' => false,
+                'choices' => $choices
+            )
+        );
+
+        $builder->add('submit', 'submit', array('label' => 'Continue'));
+        return $builder->getForm();
+    }
+
+    private function createRedirect($path, $options)
+    {
+        return $this->redirect($this->generateUrl($path, $options));
+    }
+
+    private function showSelectBusinessForm($form)
+    {
+        return $this->render(
+            "MFBAdminBundle:SetupWizard:selectBusiness.html.twig",
+            array('form' => $form->createView())
+        );
+    }
 }
